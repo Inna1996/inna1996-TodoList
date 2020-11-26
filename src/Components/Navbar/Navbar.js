458dd6759784dfc32f702bf8876d06d82fcc1a89
Ina Zimmermann <inazimmermann1996@gmail.com>
2020-11-26 17:57:31 +0100

    styles for component Navbar fixed

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -6,7 +6,8 @@ import translate from './../../i18n/translate';
 const Navbar = () => {
     return (<div className={cl.navbar}>
         <div className={cl.link}>
-            <NavLink to='/' activeClassName={cl.activeLink}>{translate('aboutMe')}</NavLink>
+            <NavLink to='/' exact
+                activeClassName={cl.activeLink}>{translate('aboutMe')}</NavLink>
         </div>
         <div className={cl.link}>
             <NavLink to='/todo' activeClassName={cl.activeLink}>{translate('todo')}</NavLink>
